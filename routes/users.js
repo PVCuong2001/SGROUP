527b1ed6bf74cbaa5499879957bdb8f84a8eb2de
PVCuong2001 <cuongphanit2001@gmail.com>
2022-08-15 23:59:51 +0700

    init

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { getDatabaseConnection } = require('../configs/database');
-
-const connection = getDatabaseConnection();
-const dbo = connection.db('sgroup');
-const collection = dbo.collection('users');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  const data = await collection.find({}).toArray();
-  return res.status(200).json({ data });
-});
-
-router.post('/', async function (req, res, next) {
-  const {
-    name,
-    age,
-    gender,
-  } = req.body;
-
-  const connection = getDatabaseConnection();
-  const dbo = connection.db('sgroup');
-  const collection = dbo.collection('users');
-  await collection.insertOne({
-    name,
-    age,
-    gender,
-  });
-  return res.status(200).json({ message: 'success' });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
